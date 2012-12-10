5ff1727e13c46363879ab9fff9b977b1132bb0fe
Naoto Ikenaga <free-travel@hotmail.co.jp>
2012-12-10 17:47:34 +0900

    table.jsからspotwin.jsへのwin操作をシングルコンテキストに対応。

--- Resources/spotwin.js
+++ Resources/spotwin.js
@@ -1,15 +1,27 @@
-Ti.UI.currentWindow.title = "スポット情報";
-Ti.UI.currentWindow.backgroundColor = "#fff"; 
+function spotWin(){
+	var spotwin = Ti.UI.createWindow({
+		backgroundColor: "#ff0" 
+	});
 
-var view_spotwin = Ti.UI.createView();
+	var view_spotwin = Ti.UI.createView();
 
-var labelTest = Ti.UI.createLabel({
-    color: '#999',
-    text: 'spotdata',
-    height: 32,
-    width: 150,
-    top: 120
-});
-view_spotwin.add(labelTest);
-Ti.UI.currentWindow.add(view_spotwin);
+	var id = Ti.App.toSpotwinId;
 
+	var labelTest = Ti.UI.createLabel({
+	    color: '#999',
+	    text: 'spotdata',
+	    height: 320,
+	    width: 200,
+	    top: 50
+	});
+
+	view_spotwin.add(labelTest);
+	
+	labelTest.text = Ti.App.spots.spotdata[id].id + "\n" + Ti.App.spots.spotdata[id].name + "\n" + Ti.App.spots.spotdata[id].hint1 + "\n" + Ti.App.spots.spotdata[id].hint2 + "\n" + Ti.App.spots.spotdata[id].hint3 + "\n" + Ti.App.spots.spotdata[id].web + "\n" + Ti.App.spots.spotdata[id].address + "\n" + Ti.App.spots.spotdata[id].info;
+	
+	spotwin.add(view_spotwin);
+
+	return spotwin;
+}
+
+module.exports = spotWin;
--- Resources/table.js
+++ Resources/table.js
@@ -3,8 +3,12 @@ Ti.UI.currentWindow.backgroundColor = "#fff";
 
 var view_table =Ti.UI.createView();
 
-var nearData = new Array();
-chkDist(Ti.App.cFinLat,Ti.App.cFinLong,Ti.App.spots.spotdata);
+var nearData = new Array(); //周辺データを保管する配列
+var earth_r = 6378.137;
+
+chkDist(Ti.App.cFinLat,Ti.App.cFinLong,Ti.App.spots.spotdata);//nearDataを操作
+
+nearData.sort(renSortFunc);//nearDataを並び替え
 
 var table = Ti.UI.createTableView({
 	data:createTableData(nearData)
@@ -12,21 +16,33 @@ var table = Ti.UI.createTableView({
 
 /* テーブルをクリックでタブが開く */
 table.addEventListener('click', function(e){
-    if (e.rowData.hasChild) {
-        var w = Ti.UI.createWindow({
-            url: e.rowData.dest,
-            title: e.rowData.title,
-            backgroundColor: 'white'
-        });
-        Ti.UI.currentTab.open(w);
-    }
+	if (e.rowData.hasChild) {
+		Ti.App.toSpotwinId = nearData[e.index].nearId;
+		var Win = require('spotwin');
+		w = new Win();
+		w.title = "スポット情報";
+		w.barColor = '#000';
+
+		var b = Titanium.UI.createButton({
+			title:'Close',
+			style:Titanium.UI.iPhone.SystemButtonStyle.PLAIN
+		});
+		/* wにボタン追加 */
+		w.setLeftNavButton(b);
+		b.addEventListener('click',function()
+		{
+			w.close();
+		});
+		/* wをオープンさせる。modalアニメ付き。 */	
+		w.open({modal:true});
+	}
 });
 
 view_table.add(table);
 
 Ti.UI.currentWindow.add(view_table);
 
-/* JSONから差が3km以内だったら名前を出す */
+/* JSONから差が4km以内だったら名前を出す */
 function chkDist(cLat,cLong,spotArr){
 	var dist = 0;
 	for(var i=0;i<spotArr.length;i++){
@@ -35,7 +51,6 @@ function chkDist(cLat,cLong,spotArr){
 			nearData.push({nearId:spotArr[i].id,nearDist:dist});
 		}
 	}
-	Ti.API.info(nearData);
 }
 
 /* 連想配列のソート  */
@@ -43,7 +58,6 @@ function renSortFunc(a,b) {return (a.nearDist > b.nearDist) ? 1 : -1;}
 
 /* 2点間の距離を出す */
 function calDist(cLat,cLong,bLat,bLong){
-	var earth_r = 6378.137;
 
 	var latSa = deg2rad(bLat - cLat);
 	var longSa = deg2rad(bLong - cLong);
@@ -58,17 +72,13 @@ function calDist(cLat,cLong,bLat,bLong){
 function createTableData(dataArr){
 	var tmpDataArr = new Array();
 	
-	//並び替え
-	dataArr.sort(renSortFunc);
-	Ti.API.info(dataArr);
 	//テーブルデータの作成
 	for(var i=0;i<dataArr.length;i++){
 		var tableTitle = dataArr[i].nearDist + "m先：" + Ti.App.spots.spotdata[dataArr[i].nearId].hint1;
 		tmpDataArr.push({
 			title: tableTitle,
 			hasChild:true,
-			color: "#000000",
-			dest:"spotwin.js"
+			color: "#000000"
 		});
 	}
 	return tmpDataArr;
